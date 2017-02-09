f780d96847416e7d5f3ec1e2eb803fd00406bacb
Krishn Ramesh <krishnr@gmail.com>
2017-02-09 01:02:59 -0500

    plotted contours

--- Lab 1/Cluster-Generation/class_data.m
+++ Lab 1/Cluster-Generation/class_data.m
@@ -3,27 +3,34 @@
 % Febuary 12 2017
 %
 % Brady Kieffer - 20517665
+% Krishn Ramesh - 20521942
 
 % Case 1
 clear;
+close all;
 
 n_a = 200; 
 mu_a = [5, 10];
 covar_a = [8, 0; 0, 4];
+[eig_vecs_a, eig_vals_a] = eig(covar_a); 
 
 n_b = 200; 
 mu_b = [10, 15];
 covar_b = [8, 0; 0, 4];
+[eig_vecs_b, eig_vals_b] = eig(covar_b); 
 
 % Case 2
 n_c = 100; 
 mu_c = [5, 10];
 covar_c = [8, 4; 4, 40];
+[eig_vecs_c, eig_vals_c] = eig(covar_c);
 
 n_d = 200; 
 mu_d = [15, 10];
 covar_d = [8, 0; 0, 8];
+[eig_vecs_d, eig_vals_d] = eig(covar_d);
 
 n_e = 150; 
 mu_e = [10, 5];
 covar_e = [10, -5; -5, 20];
+[eig_vecs_e, eig_vals_e] = eig(covar_e);
--- Lab 1/Cluster-Generation/plot_all_clusters.m
+++ Lab 1/Cluster-Generation/plot_all_clusters.m
@@ -4,21 +4,60 @@
 % Febuary 12 2017
 %
 % Brady Kieffer - 20517665
+% Krishn Ramesh - 20521942
 
-class_data; % Creates the class data
+%% Creates the class data
+class_data;
+
 samples_a = bivariate_normal(n_a, covar_a, mu_a);
 samples_b = bivariate_normal(n_b, covar_b, mu_b);
 
-x1 = [1:1:200];
+samples_c = bivariate_normal(n_c, covar_c, mu_c);
+samples_d = bivariate_normal(n_d, covar_d, mu_d);
+samples_e = bivariate_normal(n_e, covar_e, mu_e);
 
-% Case 1
+%% Case 1
+close all;
+
 hold on;
 scatter(samples_a(:, 1), samples_a(:, 2), 'rx');
 scatter(samples_b(:, 1), samples_b(:, 2), 'bo');
 
-plot(mu_a(1), mu_a(2), 'go');
-plot(mu_b', 'gx');
+plot(mu_a(1), mu_a(2), 'gx');
+plot(mu_b(1), mu_b(2), 'go');
+
+theta_a = atan(eig_vecs_a(2,2)/eig_vecs_a(2,1));
+plot_ellipse(mu_a(1),mu_a(2), theta_a,covar_a(1,1),covar_a(2,2), 'r');
+
+theta_b = atan(eig_vecs_b(2,2)/eig_vecs_b(2,1));
+plot_ellipse(mu_b(1),mu_b(2), theta_b,covar_b(1,1),covar_b(2,2), 'b');
 
 title('Plot of Samples of Class A and Class B');
 legend('Class A', 'Class B', 'Location', 'northeast');
+hold off;
+
+%% Case 2
+close all;
+
+hold on;
+
+scatter(samples_c(:, 1), samples_c(:, 2), 'rx');
+scatter(samples_d(:, 1), samples_d(:, 2), 'bo');
+scatter(samples_e(:, 1), samples_e(:, 2), 'k+');
+
+plot(mu_c(1), mu_c(2), 'gx');
+plot(mu_d(1), mu_d(2), 'go');
+plot(mu_e(1), mu_e(2), 'g+');
+
+theta_c = atan(eig_vecs_c(1,2)/eig_vecs_c(1,1));
+plot_ellipse(mu_c(1),mu_c(2), theta_c,covar_c(1,1),covar_c(2,2), 'r');
+
+theta_d = atan(eig_vecs_d(1,2)/eig_vecs_d(1,1));
+plot_ellipse(mu_d(1),mu_d(2), theta_d,covar_d(1,1),covar_d(2,2), 'b');
+
+theta_e = atan(eig_vecs_e(1,2)/eig_vecs_e(1,1));
+plot_ellipse(mu_e(1),mu_e(2), theta_e,covar_e(1,1),covar_e(2,2), 'k');
+
+title('Plot of Samples of Class C, Class D & Class E');
+legend('Class C', 'Class D', 'Class E', 'Location', 'northeast');
 hold off;
